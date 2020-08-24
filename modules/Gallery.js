e4e16d87088635c0a38fa505498d5e5741d82762
Ebenezer Don <ebenezerdonu@gmail.com>
2020-08-24 21:15:44 +0100

    add photo input button

--- modules/Gallery.js
+++ modules/Gallery.js
@@ -1,4 +1,8 @@
 const Gallery = () => `
+  <input type="file" name="photo" id="addPhotoInput">
+  <label for="addPhotoInput">
+    <i class="add-photo fas fa-plus-square"></i>
+  </label>
   <div class="gallery-nav"></div>
   <section class="gallery">
     <a href="#" class="item">
