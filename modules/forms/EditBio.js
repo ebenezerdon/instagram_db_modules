7b84657b883c2a18ad675035f1075901fdc82ba8
Ebenezer Don <ebenezerdonu@gmail.com>
2020-09-07 00:43:09 +0100

    add EditBio form module

---
+++ modules/forms/EditBio.js
@@ -0,0 +1,12 @@
+const EditBio = () => {
+  return `
+    <form class="edit-bio-form">
+      <input type="text" id="name" placeholder="Name" />
+      <input type="text" id="about" placeholder="about" /> </br>
+      <button>Cancel</button>
+      <button type="submit">Submit</button>
+    </form>
+  `
+}
+
+export default EditBio
