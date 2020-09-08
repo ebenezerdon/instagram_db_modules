6bab0c1b92dfa47585a3ffee0fad3067d4417325
Ebenezer Don <ebenezerdonu@gmail.com>
2020-09-08 16:48:09 +0100

    use userInfo in Bio function

--- modules/bio/Bio.js
+++ modules/bio/Bio.js
@@ -3,15 +3,15 @@ import { getEntryFromDb } from '../../database.js'
 
 const Bio = async () => {
   const userInfo = await getEntryFromDb('bio')
-  console.log(userInfo)
+  console.log({userInfo})
   return `
   <section class="bio">
     <div class="profile-photo">
       <img src="https://images.pexels.com/photos/1727280/pexels-photo-1727280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="profile-photo">
     </div>
     <div class="profile-info">
-      <p class="name">Jane Butters</p>
-      <p class="about">Hi, I'm Jane. I used to be indecisive but now I’m not so sure.</p>
+      <p class="name">${userInfo[0].name}</p>
+      <p class="about">${userInfo[0].about}</p>
       <button class="edit-bio-button"}>Edit bio</button>
     </div>
     ${EditBioForm()}
