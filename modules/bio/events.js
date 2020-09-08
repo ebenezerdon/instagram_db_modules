ffcab7e12116087f3e8955778b2712971051c597
Ebenezer Don <ebenezerdonu@gmail.com>
2020-09-08 04:53:06 +0100

    add event listener for cancel edit bio form

--- modules/bio/events.js
+++ modules/bio/events.js
@@ -1,19 +1,24 @@
-import { addEntryToDb } from '../../database.js';
+import { addEntryToDb, clearAllEntries } from '../../database.js';
 
 const addBioEventListeners = () => {
   const editBioButton = document.querySelector('.edit-bio-button')
   const editBioForm = document.querySelector('.edit-bio-form')
+  const cancelFormButton = document.querySelector('.cancel-edit-bio')
 
   editBioButton.addEventListener('click', () => {
-    const form = document.querySelector('.edit-bio-form')
-    form.style.display = 'block'
+    editBioForm.style.display = 'block'
   })
 
   editBioForm.addEventListener('submit', () => {
     event.preventDefault()
     const name = document.querySelector('#name').value
     const about = document.querySelector('#about').value
+    clearAllEntries('bio')
     addEntryToDb('bio', { name, about })
+  })
+
+  cancelFormButton.addEventListener('click', () => {
+    editBioForm.style.display = 'none'
   })
 }
 
