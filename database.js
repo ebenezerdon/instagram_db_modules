cb6bd4a2c11dbae10ae3c68a7d7aef353a908810
Ebenezer Don <ebenezerdonu@gmail.com>
2020-09-08 16:36:23 +0100

    return promise in getEntryFromDb method
    - refactor database assignment

--- database.js
+++ database.js
@@ -1,54 +1,55 @@
-let database
-
+// let database
 const initializeDb = indexedDB.open('instagram', 1)
 
-initializeDb.onsuccess = event => {
-  database = event.target.result
-  console.log('Success creating or accessing db')
-}
+// initializeDb.onsuccess = () => {
+//   database = initializeDb.result
+//   console.log('Success creating or accessing db')
+// }
 
-initializeDb.onupgradeneeded = event => {
-  database = event.target.result
+initializeDb.onupgradeneeded = () => {
+  const database = initializeDb.result
   database.createObjectStore('bio', {autoIncrement: true})
   database.createObjectStore('gallery', {autoIncrement: true})
 }
 
-initializeDb.onerror = event => {
+initializeDb.onerror = () => {
   alert('Error creating or accessing db')
-  console.log(event.target)
 }
 
 const addEntryToDb = (storeName, entry) => {
+  const database = initializeDb.result
   const transaction = database.transaction([storeName], 'readwrite')
   const store = transaction.objectStore(storeName)
   store.add(entry)
 
   transaction.oncomplete = () => alert(`Entry added to ${storeName}!`)
-  transaction.onerror = event => {
+  transaction.onerror = () => {
     console.log(`error adding Entry to ${storeName}.`)
-    console.log(event.target.error);
+    console.log(transaction.error);
   }
 }
 
 const getEntryFromDb = (storeName, id) => {
-  const transaction = initializeDb.result.transaction([storeName])
-  const store = transaction.objectStore(storeName)
-  const request = id ? store.get(id) : store.getAll()
-  request.onerror = () => {
-    console.log('error getting data from the store');
-  }
+  const data = new Promise((resolve, reject) => {
+    const database = initializeDb.result
+    const transaction = database.transaction([storeName])
+    const store = transaction.objectStore(storeName)
+    const request = id ? store.get(id) : store.getAll()
+    request.onerror = () => {
+      reject(request.error)
+      console.log('error getting data from the store');
+    }
 
-  request.onsuccess = () => {
-    console.log('success', request.result)
-    return request.result
-  }
+    request.onsuccess = () => {
+      resolve(request.result)
+    }
+  })
 
-  return setTimeout(() => {
-    request.onsuccess()
-  }, 200);
+  return Promise.resolve(data)
 }
 
 const clearAllEntries = (storeName) => {
+  const database = initializeDb.result
   const transaction = database.transaction([storeName], 'readwrite')
   const store = transaction.objectStore(storeName)
   store.clear()
