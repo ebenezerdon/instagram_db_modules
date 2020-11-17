b0f3f7526c30d5765adf8668570d07266a4de5c9
Ebenezer Don <ebenezerdonu@gmail.com>
2020-11-17 03:33:55 +0100

    update dom in realtime when an image is uploaded

--- modules/gallery/events.js
+++ modules/gallery/events.js
@@ -2,20 +2,29 @@ import { addEntryToDb, getEntryFromDb } from '../../database.js'
 
 const addGalleryEventListeners = () => {
   const photoInput = document.querySelector('#addPhotoInput')
+  const gallerySection = document.querySelector('.gallery')
   photoInput.addEventListener('change', () => {
     const reader = new FileReader()
     reader.readAsDataURL(photoInput.files[0])
 
     reader.addEventListener('load', () => {
       addEntryToDb('gallery', reader.result)
-    })
+
+      const newItem = `
+      <a href="#" class="item">
+        <img alt="stuff" src=${reader.result}>
+      </a>
+    `
+    gallerySection.innerHTML = newItem + gallerySection.innerHTML
+
+  })
   })
 }
 
 const addImagesToGallery = async () => {
   const gallerySection = document.querySelector('.gallery')
   const galleryData = await getEntryFromDb('gallery')
-  const galleryItems = galleryData.map(singlePhoto => {
+  const galleryItems = galleryData.reverse().map(singlePhoto => {
     console.log({singlePhoto});
     return `
       <a href="#" class="item">
