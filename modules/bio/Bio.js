332e01c9c65b7fec20df76e1cf8c7bdf6d262dfe
Ebenezer Don <ebenezerdonu@gmail.com>
2020-11-17 03:33:30 +0100

    add message if no userInfo

--- modules/bio/Bio.js
+++ modules/bio/Bio.js
@@ -2,16 +2,15 @@ import EditBioForm from './EditBio.js'
 import { getEntryFromDb } from '../../database.js'
 
 const Bio = async () => {
-  const userInfo = await getEntryFromDb('bio')
-  console.log({userInfo})
+  const userInfo = await getEntryFromDb('bio') || []
   return `
   <section class="bio">
     <div class="profile-photo">
       <img src="https://images.pexels.com/photos/1727280/pexels-photo-1727280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="profile-photo">
     </div>
     <div class="profile-info">
-      <p class="name">${userInfo[0].name}</p>
-      <p class="about">${userInfo[0].about}</p>
+      <p class="name">${userInfo[0] ? userInfo[0].name : 'Please update your bio'}</p>
+      <p class="about">${userInfo[0]? userInfo[0].about : ''}</p>
       <button class="edit-bio-button"}>Edit bio</button>
     </div>
     ${EditBioForm()}
